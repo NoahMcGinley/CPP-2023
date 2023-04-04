653712b2e2af94e998c4c73765d011f9eaecb671
NoahMcGinley <91349694+NoahMcGinley@users.noreply.github.com>
2023-04-04 14:49:01 -0500

    track project

--- Chapter 9/addAthlete2/addAthlete2/addAthlete2.cpp
+++ Chapter 9/addAthlete2/addAthlete2/addAthlete2.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 using namespace std;
 
-void readFile();
-//void updateFile(vector<string>);
 void updateFile(string[], int);
 void addAthlete();
 
@@ -20,81 +18,13 @@ int main()
 ----------------------------------*/
 void addAthlete()
 {
-	//readFile();
-	const int SIZE = 3;
-	string arry[SIZE] = {"a b", "c d", "d e"};
-	updateFile(arry, SIZE);
-}
-
-
-/*--------------------------------
-# reafFile accepts no arguments
-# It opens the file of athletes
-# and puts the names into a vector
-----------------------------------*/
-void readFile()
-{
-	// Variables and files
-	ifstream athleteFile(R"(C:\Users\SNM00013\Desktop\CPP-2022\files\data_files\demoAthletes.txt)");
-	vector<string> nameList;
-	string name;
-	string newAthlete;
-	int i = 0;
-
-	// Validates file
-	if (!athleteFile)
+	const int SIZE = 4;
+	string arry[4][3][4] =
 	{
-		cout << "File not found.\n";
-	}
-	else
-	{
-		// Reads file to add to vector
-		while (getline(athleteFile, name))
-		{
-			nameList.push_back(name);
-			i++;
-		}
-		//updateFile(nameList);
-	}
-	athleteFile.close();
-
+				 { {"a", "b", "c", "d"}, {"e", "f", "g", "h"}},
+				 { {"i", "j", "k", "l"}, {"m", "n", "o", "p"}, {"q", "r", "s", "t"}}
+	};
 }
-
-
-///*--------------------------------
-//# addAthlete accepts a vector string
-//# It asks the name of the new athlete
-//# and adds it to the vector. It then
-//# writes the updated vector to the file
-//----------------------------------*/
-//void updateFile(vector<string> nameList)
-//{
-//	// Variables and files
-//	ofstream athleteFile(R"(C:\Users\SNM00013\Desktop\CPP-2022\files\data_files\demoAthletes.txt)");
-//	string newAthlete;
-//	int i = 0;
-//
-//	// Validates file
-//	if (!athleteFile)
-//	{
-//		cout << "File not found";
-//	}
-//	else
-//	{
-//		// Asks user for the name of the new athlete
-//		cout << "Enter the name of the athlete you would like to enter: ";
-//		getline(cin, newAthlete);
-//		nameList.push_back(newAthlete);
-//
-//		// Loops through vector to write it to the file
-//		for (string name : nameList)
-//		{
-//			athleteFile << nameList[i] << endl;
-//			i++;
-//		}
-//	}
-//	athleteFile.close();
-//}
 
 
 /*--------------------------------
@@ -123,8 +53,8 @@ void updateFile(string athleteArry[], int SIZE)
 		// Loops through vector to write it to the file
 		for (int i = 0; i < SIZE; i++)
 		{
+			cout << athleteArry[i] << endl;
 			athleteFile << athleteArry[i] << endl;
-			i++;
 		}
 		athleteFile << newAthlete << endl;
 
